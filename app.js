c127159ea2f01bc0b81e96433c94b13617b71e7f
Prajwal Chandanagiri <prajwal8357@gmail.com>
2022-05-10 21:48:39 +0530

    Clock Running Succesfully

---
+++ app.js
@@ -0,0 +1,9 @@
+const options ={weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }; 
+
+setInterval(() => {
+  let a, date, time;
+  a = new Date();
+  date = a.toLocaleDateString(undefined,options);
+  time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById("time").innerHTML = time + "  on " + date;
+}, 1000);
